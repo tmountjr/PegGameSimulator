59ae13e5fe76168393f83b596c84935110ce3d45
tmountjr <tmountjr@gmail.com>
2014-02-26 21:23:03 -0500

    switched to boilerplate min.'s

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+/*
+ * In this file:
+ * --hover over a piece, show available moves (neighbor and destination in bold; change current market to 'O', neighbor marker to 'x', and destination marker to 'P'
+ * --track two clicks: source and destination.
+ * --new form for a move string (kinda like chess)
+ * --create move string from clicks and put it in text box
+ * --submit form with that text box
+ * --submit move to class for execution
+ * --return as normal
+*/
