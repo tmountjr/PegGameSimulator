4283ea07148eed7429a033f6040d85cecbe09c03
tmountjr <tmountjr@gmail.com>
2014-02-26 23:08:40 -0500

    yeah, it works.

--- PegGameSimulator.class.php
+++ PegGameSimulator.class.php
@@ -153,31 +153,27 @@ class PegGameSimulator
 		$table = array();
 		$max_col = ($this->row_count * 2) - 1;
 		$cell_id = 1;
+		
+		$r = "<table>\n\t<tbody>";
 		foreach ($this->rows as $row_id => $row) {
+			$r .= "\t\t<tr>\n";
 			$pcell = ($this->row_count + 1) - $row_id;
 			$pcount = 0;
 			for ($i = 1; $i <= $max_col; $i++) {
+				$r .= "\t\t\t<td>";
 				if ($pcount < $row_id && $i == $pcell) {
-					$table[$row_id][$i] = $this->game_board[$cell_id];
+					$r .= "<p class='cell'>" . $this->game_board[$cell_id] . "</p>";
 					$cell_id++;
 					$pcount++;
 					$pcell += 2;
-				} else {
-					$table[$row_id][$i] = "";
 				}
-			}
-		}
-		
-	    $r = "<table>\n\t<tbody>";
-		foreach ($table as $row_id => $row) {
-			$r .= "\t\t<tr>\n";
-			foreach ($row as $cell) {
-				$r .= "\t\t\t<td>$cell</td>\n";
+				$r .= "</td>\n";
 			}
 			$r .= "\t\t</tr>\n";
 		}
-	    $r .= "\t</tbody>\n</table>\n";
-	    return $r;
+		$r .= "\t</tbody>\n</table>\n";
+		
+		return $r;
 	}
 	
 	public function MakeMove($use_logic = false)
